1afe773eeb97105c180a04bad4b2ede61b830b5f
Red-Cat-Fat <boronnikov_a_i@mail.ru>
2018-12-23 18:18:17 +0300

    Create class Connector and TankController

---
+++ RoboController/Connector.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Threading;
+
+namespace RoboController
+{
+    public class Connector
+    {
+        private SerialPort _serialPort;
+        private bool _continue = false;
+        private Thread readThread;
+        public const int portBaudRate = 9600;
+        public Queue<string> messages = new Queue<string>();
+
+        private void initianalPort()
+        {
+            _serialPort = new SerialPort();
+            _continue = false;
+        }
+
+        public void ConnectToPort(string portName)
+        {
+            initianalPort();
+            TryClosePort();
+            readThread = new Thread(Read);
+            _serialPort.PortName = SetPortName(portName);
+            _serialPort.BaudRate = portBaudRate;
+            _serialPort.ReadTimeout = 500;
+            _serialPort.WriteTimeout = 500;
+
+            _serialPort.Open();
+            _continue = true;
+            readThread.Start();
+        }
+
+        public void TryClosePort()
+        {
+            if (_continue)
+            {
+                readThread.Join();
+                _serialPort.Close();
+                _continue = false;
+            }
+        }
+
+        public string SetPortName(string portName)
+        {
+            if (portName == "" || !(portName.ToLower()).StartsWith("com"))
+            {
+                portName = _serialPort.PortName;
+            }
+            return portName;
+        }
+
+        public void Read()
+        {
+            while (_continue)
+            {
+                try
+                {
+                    string message = _serialPort.ReadLine();
+                    messages.Enqueue(message);
+                }
+                catch (TimeoutException) { }
+            }
+        }
+
+        public string GetMessage()
+        {
+            if (messages.Count > 0)
+            {
+                return messages.Dequeue();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public void Write(char message)
+        {
+            _serialPort.Write(String.Format("{0}", message));
+        }
+    }
+}
---
+++ RoboController/TankController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboController
+{
+    public enum TankCommand
+    {
+        NO_COMMAND,
+        COMMAND_UP,
+        COMMAND_DOWN,
+        COMMAND_LEFT,
+        COMMAND_RIGTH,
+        COMMAND_TURRET_LEFT,
+        COMMAND_TURRET_RIGHT,
+        COMMAND_MOVE_GUN
+    }
+    public class TankController
+    {
+        private Connector _connector= new Connector();
+        private TankCommand _currentCommand = TankCommand.NO_COMMAND;
+        public TankController(string portName)
+        {
+            _connector.ConnectToPort(portName);
+        }
+
+        public void SetCommand(TankCommand tankCommand)
+        {
+            if (tankCommand != _currentCommand)
+            {
+                _currentCommand = tankCommand;
+
+                sendCommand(_currentCommand);
+            }
+        }
+
+        private void sendCommand(TankCommand tankCommand)
+        {
+            switch (tankCommand)
+            {
+                case TankCommand.COMMAND_UP:
+                    {
+                        _connector.Write('w');
+                        break;
+                    }
+                case TankCommand.COMMAND_LEFT:
+                    {
+                        _connector.Write('a');
+                        break;
+                    }
+                case TankCommand.COMMAND_DOWN:
+                    {
+                        _connector.Write('s');
+                        break;
+                    }
+                case TankCommand.COMMAND_RIGTH:
+                    {
+                        _connector.Write('d');
+                        break;
+                    }
+                case TankCommand.COMMAND_TURRET_LEFT:
+                    {
+                        _connector.Write('q');
+                        break;
+                    }
+                case TankCommand.COMMAND_TURRET_RIGHT:
+                    {
+                        _connector.Write('e');
+                        break;
+                    }
+                case TankCommand.COMMAND_MOVE_GUN:
+                    {
+                        _connector.Write('c');
+                        break;
+                    }
+                case TankCommand.NO_COMMAND:
+                    {
+                        _connector.Write('r');
+                        break;
+                    }
+                default:
+                    {
+                        _connector.Write('r');
+                        break;
+                    }
+            }
+        }
+    }
+}
