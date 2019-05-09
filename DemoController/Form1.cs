1ac8810d2c907b53118c6696e58afddf3edcddef
Red-Cat-Fat <boronnikov_a_i@mail.ru>
2019-05-09 20:07:28 +0300

    Обновил табуляцию

--- DemoController/Form1.cs
+++ DemoController/Form1.cs
@@ -1,47 +1,47 @@
-﻿using System;
+﻿using RoboController;
+using System;
 using System.Windows.Forms;
-using RoboController;
 
 namespace DemoController
 {
-    public partial class Form1 : Form
-    {
-        private Connector _connector;
-        private TankController _tankController;
+	public partial class Form1 : Form
+	{
+		private Connector _connector;
+		private TankController _tankController;
 
-        public Form1()
-        {
-            InitializeComponent();
-        }
+		public Form1()
+		{
+			InitializeComponent();
+		}
 
-        private void _buttonConnect_Click(object sender, EventArgs e)
-        {
-            _tankController = new TankController(_textBoxComPort.Text);
-            if (_tankController.IsConnected)
-            {
-                _buttonConnect.Text = "Подключено";
-                _textBoxComPort.Visible = false;
-            }
-        }
+		private void _buttonConnect_Click(object sender, EventArgs e)
+		{
+			_tankController = new TankController(_textBoxComPort.Text);
+			if (_tankController.IsConnected)
+			{
+				_buttonConnect.Text = "Подключено";
+				_textBoxComPort.Visible = false;
+			}
+		}
 
-        private void _buttonUp_Click(object sender, EventArgs e)
-        {
-            _tankController.SetCommand(TankCommand.COMMAND_UP);
-        }
+		private void _buttonUp_Click(object sender, EventArgs e)
+		{
+			_tankController.SetCommand(TankCommand.COMMAND_UP);
+		}
 
-        private void _buttonDown_Click(object sender, EventArgs e)
-        {
-            _tankController.SetCommand(TankCommand.COMMAND_DOWN);
-        }
+		private void _buttonDown_Click(object sender, EventArgs e)
+		{
+			_tankController.SetCommand(TankCommand.COMMAND_DOWN);
+		}
 
-        private void _buttonLeft_Click(object sender, EventArgs e)
-        {
-            _tankController.SetCommand(TankCommand.COMMAND_LEFT);
-        }
+		private void _buttonLeft_Click(object sender, EventArgs e)
+		{
+			_tankController.SetCommand(TankCommand.COMMAND_LEFT);
+		}
 
-        private void _buttonRight_Click(object sender, EventArgs e)
-        {
-            _tankController.SetCommand(TankCommand.COMMAND_RIGTH);
-        }
-    }
+		private void _buttonRight_Click(object sender, EventArgs e)
+		{
+			_tankController.SetCommand(TankCommand.COMMAND_RIGTH);
+		}
+	}
 }
