e1593ed207fcaca50de0f371b7e5e72580b443f7
Red-Cat-Fat <boronnikov_a_i@mail.ru>
2019-07-14 23:46:33 +0300

    Внёс исправления. После получения команды происходит проверка полученной команды и только если команда отличается от последней - выполняет её.

--- Arduino/Acula/Acula.ino
+++ Arduino/Acula/Acula.ino
@@ -1,19 +1,16 @@
-/*   Данный скетч делает следующее: передатчик (TX) отправляет массив
-     данных, который генерируется согласно показаниям с кнопки и с
-     двух потенциомтеров. Приёмник (RX) получает массив, и записывает
-     данные на реле, сервомашинку и генерирует ШИМ сигнал на транзистор.
-    by AlexGyver 2016
-*/
-
 #include <SPI.h>
 #include "nRF24L01.h"
 #include "RF24.h"
 
+//========================================//
+//=========== Пины управления ============//
+//========================================//
 #define in1 2 //down
-#define in2 3//up
-#define in3 4//right
-#define in4 5//left
-#define in5 6//no comand
+#define in2 3 //up
+#define in3 4 //right
+#define in4 5 //left
+#define in5 6 //no comand
+
 //========================================//
 //=============== Команды ================//
 //========================================//
@@ -43,9 +40,8 @@
 int last_command = NO_COMMAND;
 byte incomingByte = 0;
 
-
-void set_command(int Comand) {
-  switch (Comand) {
+void set_command(int comand) {
+  switch (comand) {
     case COMMAND_UP:
       ACTION_COMMAND_UP();
       break;
@@ -105,6 +101,8 @@ void init_pins() {
   pinMode(in3, OUTPUT);      // устанавливает режим работы - выход
   pinMode(in4, OUTPUT);      // устанавливает режим работы - выход
   pinMode(in5, OUTPUT);      // устанавливает режим работы - выход
+
+  ALL_PINS_LOW();
 }
 
 void setup() {
@@ -113,38 +111,37 @@ void setup() {
 }
 
 void loop() {
-  // считываем байт
-
   if (Serial.available() <= 0) {
     incomingByte = Serial.read();
-    last_command = incomingByte;
   }
-  switch (last_command) {
-    case KEY_W: {
-        set_command(COMMAND_UP);
+  if (last_command != incomingByte) {
+    last_command = incomingByte;
+    switch (last_command) {
+      case KEY_W: {
+          set_command(COMMAND_UP);
+          break;
+        }
+      case KEY_A: {
+          set_command(COMMAND_LEFT);
+        }
+      case KEY_S: {
+          set_command(COMMAND_DOWN);
+          break;
+        }
+      case KEY_D: {
+          set_command(COMMAND_RIGTH);
+          break;
+        }
+      case KEY_X: {
+          set_command(NO_COMMAND);
+          break;
+        }
+      case KEY_R: {
+          set_command(NO_COMMAND);
+          break;
+        }
+      default:
         break;
-      }
-    case KEY_A: {
-        set_command(COMMAND_LEFT);
-      }
-    case KEY_S: {
-        set_command(COMMAND_DOWN);
-        break;
-      }
-    case KEY_D: {
-        set_command(COMMAND_RIGTH);
-        break;
-      }
-    case KEY_X: {
-        set_command(NO_COMMAND);
-        break;
-      }
-    case KEY_R: {
-        set_command(NO_COMMAND);
-        break;
-      }
-    default:
-      break;
+    }
   }
 }
-
